c3277938127fdda4ad89ea341eb19d0082843bc4
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-12-15 17:12:13 +0100

    Menu opcioness y Repositorio arreglado

--- app/src/main/java/com/jhorje18/quicktrade/MainActivity.java
+++ app/src/main/java/com/jhorje18/quicktrade/MainActivity.java
@@ -2,6 +2,9 @@ package com.jhorje18.quicktrade;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Crear menu opciones superior
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_opciones, menu);
+
+        return true;
+    }
+
+    //Eventos botones menu opciones superior
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mnNuevoUser:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
