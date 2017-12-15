edb33d17df04beb9800ccd11be94a238b3376cf9
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-12-15 16:58:48 +0100

    JSON Usuario

--- app/src/main/java/com/jhorje18/quicktrade/model/Usuario.java
+++ app/src/main/java/com/jhorje18/quicktrade/model/Usuario.java
@@ -13,6 +13,60 @@ public class Usuario {
     String CORREO;
     String DIRECCION;
 
+    public Usuario(String USUARIO, String NOMBRE, String APEDILLOS, String CORREO, String DIRECCION) {
+        this.USUARIO = USUARIO;
+        this.NOMBRE = NOMBRE;
+        this.APEDILLOS = APEDILLOS;
+        this.CORREO = CORREO;
+        this.DIRECCION = DIRECCION;
+    }
 
+    public String getUSUARIO() {
+        return USUARIO;
+    }
 
+    public void setUSUARIO(String USUARIO) {
+        this.USUARIO = USUARIO;
+    }
+
+    public String getNOMBRE() {
+        return NOMBRE;
+    }
+
+    public void setNOMBRE(String NOMBRE) {
+        this.NOMBRE = NOMBRE;
+    }
+
+    public String getAPEDILLOS() {
+        return APEDILLOS;
+    }
+
+    public void setAPEDILLOS(String APEDILLOS) {
+        this.APEDILLOS = APEDILLOS;
+    }
+
+    public String getCORREO() {
+        return CORREO;
+    }
+
+    public void setCORREO(String CORREO) {
+        this.CORREO = CORREO;
+    }
+
+    public String getDIRECCION() {
+        return DIRECCION;
+    }
+
+    public void setDIRECCION(String DIRECCION) {
+        this.DIRECCION = DIRECCION;
+    }
+
+    public String toString(){
+        return USUARIO + "{" +
+                "CORREO='" + CORREO + "\'," +
+                "NOMBRE='" + NOMBRE + "\'," +
+                "APEDILLOS='" + APEDILLOS + "\'" +
+                "DIRECCION='" + DIRECCION + "\'" +
+                "}";
+    }
 }
