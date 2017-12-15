4236ef5d86ec7d65e8dfdc479454078d39a32536
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-12-15 16:52:42 +0100

    Initial commit

---
+++ app/src/main/java/com/jhorje18/quicktrade/model/Usuario.java
@@ -0,0 +1,18 @@
+package com.jhorje18.quicktrade.model;
+
+/**
+ * Created by JHORJE on 15/12/17.
+ */
+
+public class Usuario {
+
+    //Variables
+    String USUARIO;
+    String NOMBRE;
+    String APEDILLOS;
+    String CORREO;
+    String DIRECCION;
+
+
+
+}
