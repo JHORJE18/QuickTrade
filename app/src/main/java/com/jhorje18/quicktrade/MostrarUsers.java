30ec379a96a1f8d757ca24b8de27f7dd49ffea7e
Jorge Lopez Gil <wiijlg@hotmail.com>
2017-12-20 14:56:01 +0100

    Menu en mostrar usuarios

--- app/src/main/java/com/jhorje18/quicktrade/MostrarUsers.java
+++ app/src/main/java/com/jhorje18/quicktrade/MostrarUsers.java
@@ -1,7 +1,11 @@
 package com.jhorje18.quicktrade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -60,5 +64,27 @@ public class MostrarUsers extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_opciones, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.mnNuevoUser:
+                Intent nuevo = new Intent(this,NuevoUser.class);
+                startActivity(nuevo);
+                break;
+            case R.id.mnMostrarUser:
+                recreate();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
